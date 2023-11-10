d6262fe268204f684f8224901f8c97800ef9ff28
Maximiliano Luna <maxiespn21@gmail.com>
2023-11-10 10:18:54 -0300

    docs: Change texts from Spanish to English

--- src/test/java/taskManagerApi/models/TaskEntityValidationTest.java
+++ src/test/java/taskManagerApi/models/TaskEntityValidationTest.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// This class contains tests to validate the constraints of TaskEntity class.
 class TaskEntityValidationTest {
 
     private Validator validator;
